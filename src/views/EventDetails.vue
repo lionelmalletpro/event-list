99db6b31d65443d2ba07ae7a8f0a605ad97bbb98
LionelMallet <lionel.mallet.pro@gmail.com>
2021-08-21 21:04:21 +0200

    init

---
+++ src/views/EventDetails.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="event-card" v-if="event">
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventService from "@/services/EventService.js";
+
+export default {
+  name: "EventDetails",
+  props: ["id"],
+  data() {
+    return {
+      event: null,
+    };
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then((response) => {
+        this.event = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
+</script>
+
